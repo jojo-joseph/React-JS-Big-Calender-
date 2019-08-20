425220a394ed00c8347d55d38d4f369a587b477c
jojo-joseph <jojojoseph1996@gmail.com>
2019-08-20 17:29:13 +0530

    React Big calender first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Calendarexample from './calender-component';
+function App() {
+  return (
+    <div className="App">
+      <Calendarexample />
+    </div>
+  );
+}
+
+export default App;
